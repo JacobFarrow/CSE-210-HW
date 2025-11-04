6bbcfaa2ad01e7d6fe20299300ffb403d1d2095b
Jacob Farrow <jacob-farrow@outlook.com>
2025-11-04 14:26:42 -0700

    Practise with variable types

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,8 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        /* Console.WriteLine("Hello World!");
         Console.Write("Woah.");
-        Console.WriteLine("Hi.");
+        Console.WriteLine("Hi."); */
+
+        Console.Write("What is your favouite number? ");
+        int Number = int.Parse(Console.ReadLine());
+
+        if (Number > 5 || Number < 15)
+        {
+            Console.WriteLine($"Your answer was {Number}, which is not in the range I expected");
+        }
+
+        else
+        {
+            Console.WriteLine($"Ah, {Number}. Just as I thought. It is usually between 5-14");
+        }
     }
 }
