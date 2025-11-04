1fdf2746a4acd44423ffcee818f3159046c12712
Jacob Farrow <jacob-farrow@outlook.com>
2025-11-04 14:47:20 -0700

    Prove 2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        Console.WriteLine("I have a number between 1 and 100, can you work it out?");
+        int Number;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            Number = int.Parse(Console.ReadLine());
+
+            if (magicNumber > Number)
+            {
+                Console.WriteLine("Too low");
+            }
+            else if (Number > magicNumber)
+            {
+                Console.WriteLine("Too high");
+            }
+            else
+            {
+                Console.WriteLine("That is correct.");
+            }
+        } while (Number != magicNumber);
     }
 }
