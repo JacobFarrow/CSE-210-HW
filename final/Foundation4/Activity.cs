45dc8b9a8f02b9b556eda9d7a249d4fdf8513c40
JacobFarrow <jacob-farrow@outlook.com>
2025-12-11 10:20:17 -0700

    Foundation 4

---
+++ final/Foundation4/Activity.cs
@@ -0,0 +1,29 @@
+public class Activity
+{
+    protected string _date;
+    protected int _minutes;
+
+    public Activity(string date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public virtual double GetDistance()
+    {
+        return 0;
+    }
+    public virtual double GetSpeed()
+    {
+        return 0;
+    }
+    public virtual double GetPace()
+    {
+        return 0;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_date} {this.GetType().Name} ({_minutes} min): Distance {GetDistance()} miles, Speed {GetSpeed()} mph, Pace: {GetPace()} min per mile";
+    }
+}
